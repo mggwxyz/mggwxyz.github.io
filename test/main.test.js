96ddebf7862cecef78f48754988b2e43db061120
Michael Gilbertson <mgg9xv@virginia.edu>
2017-04-16 03:33:43 -0400

    Updates

---
+++ test/main.test.js
@@ -0,0 +1,31 @@
+require('chromedriver');
+var assert = require("assert");
+// var webdriver = require('selenium-webdriver'),
+//     By = webdriver.By,
+//     until = webdriver.until;
+
+describe('mggwxyz.github.io', function(){
+
+    it('should work', function(){
+
+        assert.equal(true , true);
+
+        // var driver = new webdriver.Builder()
+        //     .forBrowser('chrome')
+        //     .build();
+        //
+        // driver.get('http://www.google.com');
+        // driver.getTitle().then(function(title) {
+        //     console.log('Title: ', title);
+        //     driver.quit();
+        //     done();
+        // });
+
+    });
+
+
+    it('should know that 1 + 1 = 2', function(){
+        assert.equal(1 + 1 , 2);
+    });
+});
+
